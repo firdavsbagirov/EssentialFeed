b942579ce7ef360449c294b9de5e71aee2daf22b
Firdavs Bagirov <firdavsbagirov@abdigital.uz>
2022-04-14 23:09:23 +0500

    create EssentialFeed framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Firdavs Bagirov on 14/04/22.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
