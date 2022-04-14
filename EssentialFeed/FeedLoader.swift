b0286a798418411f2a3a8388939b8abb62f78a59
Firdavs Bagirov <firdavsbagirov@abdigital.uz>
2022-04-14 23:12:05 +0500

    create the FeedLoader boundary

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Firdavs Bagirov on 14/04/22.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
