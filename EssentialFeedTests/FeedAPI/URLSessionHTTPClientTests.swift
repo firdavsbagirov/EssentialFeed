064695fcfd7e4066d6aa5a8a1250529657067835
Firdavs Bagirov <firdavsbagirov@abdigital.uz>
2022-05-02 19:09:45 +0500

    delivers error on all error or unexpected representation cases

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -60,17 +60,30 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_failsOnResuestError() {
         let requestError = NSError(domain: "any error", code: 1)
-        
+
         let receivedError = resultErrorFor(data: nil, response: nil, error: requestError) as NSError?
-        
+
         XCTAssertEqual(receivedError?.domain, requestError.domain)
         XCTAssertEqual(receivedError?.code, requestError.code)
 
     }
     
-    func test_getFromURL_failsOnAllNilValues() {
+    func test_getFromURL_failsOnAllInvalidRepresentationCases() {
+        let nonHTTPURLResponse = URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        let anyHTTPURLResponse = HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
+        let anyData = Data("any data".utf8)
+        let anyError = NSError(domain: "any error", code: 2)
+        
         XCTAssertNotNil(resultErrorFor(data: nil, response: nil, error: nil))
-
+        XCTAssertNotNil(resultErrorFor(data: nil, response: nonHTTPURLResponse, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: anyHTTPURLResponse, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nil, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nil, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: nonHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: anyHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nonHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: anyHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nonHTTPURLResponse, error: nil))
     }
     
     // MARK: - Helpers
@@ -91,7 +104,6 @@ class URLSessionHTTPClientTests: XCTestCase {
             switch result {
             case let .failure(error):
                 receivedError = error
-                
             default:
                 XCTFail("Expected failure, got \(result) instead", file: file, line: line)
             }
