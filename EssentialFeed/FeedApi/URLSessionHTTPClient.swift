279ec7be391e1e9d8ec08848727096ffe3fe514e
Firdavs Bagirov <firdavsbagirov@abdigital.uz>
2022-05-03 11:11:04 +0500

    enable random test execution order to guarantee there is no order dependency between test methods

--- EssentialFeed/FeedApi/URLSessionHTTPClient.swift
+++ EssentialFeed/FeedApi/URLSessionHTTPClient.swift
@@ -23,8 +23,7 @@ public class URLSessionHTTPClient: HTTPClient {
                 completion(.failure(error))
             } else if let data = data, let response = response as? HTTPURLResponse {
                 completion(.success(data, response))
-            }
-            else {
+            } else {
                 completion(.failure(UnexpectedValuesRepresentationError()))
             }
         }.resume()
