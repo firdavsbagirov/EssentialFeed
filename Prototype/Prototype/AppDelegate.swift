f12c32a5eb24a85631dfa715d2efce8a2f288efa
Firdavs Bagirov <firdavsbagirov@gmail.com>
2022-08-13 14:46:57 +0500

    Add empty project fro prototype

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Prototype
+//
+//  Created by Firdavs Bagirov on 13/08/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
