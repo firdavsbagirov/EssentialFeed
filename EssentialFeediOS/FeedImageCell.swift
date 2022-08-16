8fea091bf194fce5b400c3cdfbed3de7e668291d
Firdavs Bagirov <firdavsbagirov@gmail.com>
2022-08-16 17:26:43 +0500

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Firdavs Bagirov on 16/08/22.
+//
+
+import Foundation
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
