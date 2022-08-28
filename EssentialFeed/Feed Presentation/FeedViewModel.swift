98dd37c62c08930ec93689c685c2cd7c4dac6a5b
Firdavs Bagirov <firdavsbagirov@gmail.com>
2022-08-28 16:06:29 +0500

    Move 'FeedViewModel' to separate file

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Firdavs Bagirov on 28/08/22.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
