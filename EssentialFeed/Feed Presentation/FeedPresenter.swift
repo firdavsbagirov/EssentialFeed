43d238300c9ee327dfa322675e62c54789b6bcbe
Firdavs Bagirov <firdavsbagirov@gmail.com>
2022-08-28 16:07:08 +0500

    Move 'FeedLoadingViewModel' to separate file

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -11,10 +11,6 @@ public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
 
-public struct FeedLoadingViewModel {
-    public var isLoading: Bool
-}
-
 public protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)
 }
