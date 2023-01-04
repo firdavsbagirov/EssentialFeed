3b9b096b4ea976d78e9d759b50e272b5be7af137
Firdavs Bagirov <firdavsbagirov@gmail.com>
2023-01-04 16:09:18 +0500

    Create a lazy property to explicitly hold a reference to the `RemoteFeedLoader` instance in the `SceneDelegate`

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -25,6 +25,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }()
     
+    private let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+    
+    private lazy var remoteFeedLoader = RemoteFeedLoader(client: httpClient, url: remoteURL)
+    
     private lazy var store: FeedStore & FeedImageDataStore = {
         try! CoreDataFeedStore(
             storeURL: NSPersistentContainer
@@ -59,10 +63,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     private func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
-        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
-        
-        let remoteFeedLoader = RemoteFeedLoader(client: httpClient, url: remoteURL)
-        
         return remoteFeedLoader
             .loadPublisher()
             .caching(to: localFeedLoader)
