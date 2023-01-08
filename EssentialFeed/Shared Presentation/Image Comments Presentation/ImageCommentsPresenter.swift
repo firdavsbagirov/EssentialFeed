962ae6827d3a880b2a77ead805506d49cecdd18d
Firdavs Bagirov <firdavsbagirov@gmail.com>
2023-01-08 17:05:02 +0500

    Inject currentDate, locale, and calendar to make tests reliable in any locale

--- EssentialFeed/Shared Presentation/Image Comments Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/Shared Presentation/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -33,14 +33,15 @@ public final class ImageCommentsPresenter {
                                  comment: "Title for the image comments view")
     }
     
-    public static func map(_ comments: [ImageComment]) -> ImageCommentsViewModel {
+    public static func map(_ comments: [ImageComment], currentDate: Date = Date(), calendar: Calendar = .current, locale: Locale = .current) -> ImageCommentsViewModel {
         let formatter = RelativeDateTimeFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.calendar = calendar
+        formatter.locale = locale
         
         return ImageCommentsViewModel(comments: comments.map { comment in
             ImageCommentViewModel(
                 message: comment.message,
-                date: formatter.localizedString(for: comment.createdAt, relativeTo: Date()),
+                date: formatter.localizedString(for: comment.createdAt, relativeTo: currentDate),
                 username: comment.username)
         })
     }
