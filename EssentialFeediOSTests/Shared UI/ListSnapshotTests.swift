ba08facfef6cf4ea5a2275e8b582f6649ceaf6a8
Firdavs Bagirov <firdavsbagirov@gmail.com>
2023-01-10 18:05:03 +0500

    Replace hardcoded fonts with dynamic fonts

--- EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -20,13 +20,14 @@ class ListSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "EMPTY_LIST_dark")
     }
     
-    func test_feedWithErrorMessage() {
+    func test_listWithErrorMessage() {
         let sut = makeSUT()
         
         sut.display(.error(message: "This is a\nmulti-line\nerror message"))
         
         assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
         assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "LIST_WITH_ERROR_MESSAGE_light_extraExtraExtraLarge")
     }
     
     private func makeSUT() -> ListViewController {
