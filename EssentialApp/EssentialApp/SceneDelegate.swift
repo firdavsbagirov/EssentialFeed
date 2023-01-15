ec96ea0aa59b405f8132f34b463343eccad72a13
Firdavs Bagirov <firdavsbagirov@gmail.com>
2023-01-15 16:50:29 +0500

    Update Feed API URL

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -25,7 +25,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }()
     
-    private let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+    private let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
         
     private lazy var store: FeedStore & FeedImageDataStore = {
         try! CoreDataFeedStore(
